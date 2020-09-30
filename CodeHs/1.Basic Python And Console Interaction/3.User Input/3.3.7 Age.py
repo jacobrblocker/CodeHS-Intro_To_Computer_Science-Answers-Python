6457d5b9c1b14b6d16f31bbb3e458bbbd11b00c1
jacobrblocker <45743215+jacobrblocker@users.noreply.github.com>
2020-09-30 09:36:00 -0400

    Update 3.3.7 Age.py

--- CodeHs/1.Basic Python And Console Interaction/3.User Input/3.3.7 Age.py
+++ CodeHs/1.Basic Python And Console Interaction/3.User Input/3.3.7 Age.py
@@ -1,4 +1,4 @@
 Age = int(input("Enter your age: "))
 NumberOfCandles = Age +1
 
-print "You will need this many candles for your birthday cake:",NumberOfCandles
+print ("You will need this many candles for your birthday cake:",NumberOfCandles)
